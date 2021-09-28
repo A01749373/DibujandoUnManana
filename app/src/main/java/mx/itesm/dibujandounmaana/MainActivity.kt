32a9780549df2f30aed94c08e946a80821d55e8d
Ariadna Guzmán <58440787+A01749373@users.noreply.github.com>
2021-09-28 12:17:25 -0500

    Opciones

--- app/src/main/java/mx/itesm/dibujandounmaana/MainActivity.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_donar, R.id.nav_quienes, R.id.nav_regalos
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
