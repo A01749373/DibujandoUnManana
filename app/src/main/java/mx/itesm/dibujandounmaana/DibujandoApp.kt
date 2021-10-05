0a7f7c7a6b3b64cebd750451f8b25c6585fcf8bf
Liam Garay <65176402+A01750632@users.noreply.github.com>
2021-10-05 18:18:36 -0500

    login y sdk de Facebook

---
+++ app/src/main/java/mx/itesm/dibujandounmaana/DibujandoApp.kt
@@ -0,0 +1,10 @@
+package mx.itesm.dibujandounmaana
+
+import android.app.Application
+import com.facebook.FacebookSdk
+
+
+class DibujandoApp: Application()
+{
+    //
+}
--- app/src/main/java/mx/itesm/dibujandounmaana/view/CrearCuenta.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/view/CrearCuenta.kt
@@ -1,20 +1,40 @@
 package mx.itesm.dibujandounmaana.view
 
 import android.os.Bundle
+import android.os.ProxyFileDescriptorCallback
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.facebook.CallbackManager
 import mx.itesm.dibujandounmaana.model.Usuario
 import mx.itesm.dibujandounmaana.databinding.NavCrearCuentaBinding
 import mx.itesm.dibujandounmaana.viewmodel.CrearCuentaVM
+import com.facebook.FacebookException
+
+import com.facebook.login.LoginResult
+
+import com.facebook.FacebookCallback
+import android.content.Intent
+import com.facebook.AccessToken
+
+
+
+
+
+
+
+
+
 
 class CrearCuenta: Fragment() {
 
     private val viewModel: CrearCuentaVM by viewModels()
 
     private lateinit var binding: NavCrearCuentaBinding
+
+    lateinit var callbackManager: CallbackManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +49,20 @@ class CrearCuenta: Fragment() {
 
         configurarObservadores()
         configurarEventos()
+
+        //saber si hay un token de login
+
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+        println("logeado $isLoggedIn")
+
+        //if de las preferencias
+    }
+
+    // Resultado del callbackmanager
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun configurarEventos() {
@@ -41,6 +75,25 @@ class CrearCuenta: Fragment() {
 
             viewModel.enviarUsuario(nuevoUsuario)
         }
+        //Registra el callback de facebook
+        callbackManager = CallbackManager.Factory.create()
+
+        //Para usar el fragmento
+        binding.loginButton.setFragment(this)
+
+        binding.loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                println("Firma Exitosa")
+            }
+
+            override fun onCancel() {
+                println("Firma Cancelada")
+            }
+
+            override fun onError(exception: FacebookException) {
+                println("Firma no exitosa")
+            }
+        })
     }
 
     private fun configurarObservadores() {
