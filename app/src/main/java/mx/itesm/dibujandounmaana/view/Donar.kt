f93bcb4dd52b1a1a27600485be9eb2eea41069ab
Ariadna Guzmán <58440787+A01749373@users.noreply.github.com>
2021-10-06 10:37:40 -0500

    PayPal

--- app/src/main/java/mx/itesm/dibujandounmaana/view/Donar.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/view/Donar.kt
@@ -2,32 +2,88 @@ package mx.itesm.dibujandounmaana.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import mx.itesm.dibujandounmaana.viewmodel.DonarVM
 import mx.itesm.dibujandounmaana.R
+import mx.itesm.dibujandounmaana.databinding.DonarFragmentBinding
+import mx.itesm.dibujandounmaana.databinding.NavContactanosFragmentBinding
+import mx.itesm.dibujandounmaana.viewmodel.ContactanosVM
+import com.paypal.checkout.approve.OnApprove
+import com.paypal.checkout.createorder.CreateOrder
+import com.paypal.checkout.createorder.CurrencyCode
+import com.paypal.checkout.createorder.OrderIntent
+import com.paypal.checkout.createorder.UserAction
+import com.paypal.checkout.order.Amount
+import com.paypal.checkout.order.AppContext
+import com.paypal.checkout.order.Order
+import com.paypal.checkout.order.PurchaseUnit
+import com.paypal.checkout.paymentbutton.PayPalButton
+
 
 class Donar : Fragment() {
 
+    private val viewModel: DonarVM by viewModels()
+    private lateinit var binding: DonarFragmentBinding
     companion object {
         fun newInstance() = Donar()
     }
 
-    private lateinit var viewModel: DonarVM
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.donar_fragment, container, false)
+        binding= DonarFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //val  payPalButton = findViewById<PayPalButton>(R.id.payPalButton)
+
+        binding.payPalButton.setup(
+            createOrder = CreateOrder { createOrderActions ->
+                val order = Order(
+                    intent = OrderIntent.CAPTURE,
+                    appContext = AppContext(
+                        userAction = UserAction.PAY_NOW
+                    ),
+
+                    purchaseUnitList = listOf(
+                        PurchaseUnit(
+                            amount = Amount(
+                                currencyCode = CurrencyCode.USD,
+                                value = "10.00"
+                            )
+                        )
+                    )
+                )
+
+                createOrderActions.create(order)
+            },
+            onApprove = OnApprove { approval ->
+                approval.orderActions.capture { captureOrderResult ->
+                    Log.i("CaptureOrder", "CaptureOrderResult: $captureOrderResult")
+                    println("CaptureOrderResult: $captureOrderResult")
+
+                    //configurarObservadores()
+                    //configurarEventos()
+                }
+            })
+
+    }
+
+/*
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(DonarVM::class.java)
         // TODO: Use the ViewModel
     }
-
+*/
 }
