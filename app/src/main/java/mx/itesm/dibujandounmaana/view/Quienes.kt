e4e0ae4896e4bd83ee7f05c5f395082e5e9a5cba
Ariadna Guzmán <58440787+A01749373@users.noreply.github.com>
2021-10-07 10:37:53 -0500

    Quienes Somos

---
+++ app/src/main/java/mx/itesm/dibujandounmaana/view/Quienes.kt
@@ -0,0 +1,52 @@
+package mx.itesm.dibujandounmaana.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import mx.itesm.dibujandounmaana.R
+import mx.itesm.dibujandounmaana.databinding.NavContactanosFragmentBinding
+import mx.itesm.dibujandounmaana.databinding.QuienesFragmentBinding
+import mx.itesm.dibujandounmaana.viewmodel.ContactanosVM
+import mx.itesm.dibujandounmaana.viewmodel.QuienesVM
+
+class Quienes : Fragment() {
+
+    private val viewModel: QuienesVM by viewModels()
+    private lateinit var binding: QuienesFragmentBinding
+
+    companion object {
+        fun newInstance() = Quienes()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = QuienesFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //configurarObservadores()
+        configurarEventos()
+    }
+
+
+
+    private fun configurarEventos(){
+        binding.btnSumar.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_quienes_to_comoAyudar)
+        }
+    }
+
+
+
+}
--- app/src/main/java/mx/itesm/dibujandounmaana/viewmodel/QuienesVM.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/viewmodel/QuienesVM.kt
@@ -1,10 +1,10 @@
-package mx.itesm.dibujandounmaana.ui.gallery
+package mx.itesm.dibujandounmaana.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class QuienesVM : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
