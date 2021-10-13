b0bf38e4a8009d65d387e5b65f1211b7ea88d00e
Liam Garay <65176402+A01750632@users.noreply.github.com>
2021-10-12 20:27:42 -0500

    cambios mensaje y chats trabajo por zoom

--- app/src/main/java/mx/itesm/dibujandounmaana/chats.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/chats.kt
@@ -1,12 +1,16 @@
 package mx.itesm.dibujandounmaana
 
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -19,9 +23,13 @@ import java.util.*
 
 class chats : Fragment() {
 
+    companion object {
+        fun newInstance() = chats()
+    }
+
     private lateinit var binding: NavChatsBinding
 
-    private var user = "xmuse85@yahoo.com"
+    private var user: String = ""
     private var db = Firebase.firestore
 
     override fun onCreateView(
@@ -34,10 +42,13 @@ class chats : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val preferencias = this.requireContext().getSharedPreferences("Usuario", Context.MODE_PRIVATE)
+        val correo = preferencias.getString("Correo","-1")
+        user = correo!!
         initViews()
     }
 
-    private fun initViews(){
+    private fun initViews() {
         binding.newChatButton.setOnClickListener { newChat() }
 
         binding.listaChats.layoutManager = LinearLayoutManager(this.requireContext())
@@ -47,6 +58,7 @@ class chats : Fragment() {
             }
         val userRef = db.collection("users").document(user)
 
+        //Actualiza los chats cuando se inicia con el usuario
         userRef.collection("chats")
             .get()
             .addOnSuccessListener { chats ->
@@ -54,13 +66,19 @@ class chats : Fragment() {
 
                 (binding.listaChats.adapter as ChatAdapter).setData(listChats)
             }
-        }
+        userRef.collection("chats")
+            .addSnapshotListener { chats, error ->
+                if (error == null) {
+                    chats?.let {
+                        val listChats = it.toObjects(Chat::class.java)
+                        (binding.listaChats.adapter as ChatAdapter).setData(listChats)
+                    }
+                }
+            }
+    }
 
     private fun chatSelected(chat: Chat) {
-        val intent = Intent(this.requireContext(), Mensaje::class.java)
-        intent.putExtra("chatId", chat.id)
-        intent.putExtra("user",user)
-        startActivity(intent)
+        findNavController().navigate(R.id.action_chats_to_mensaje)
     }
 
     private fun newChat() {
@@ -78,11 +96,14 @@ class chats : Fragment() {
         db.collection("users").document(user).collection("chats").document(chatId).set(chat)
         db.collection("users").document(otherUser).collection("chats").document(chatId).set(chat)
 
-        /*
-        val intent = Intent(this.requireContext(), Mensaje::class.java)
-        intent.putExtra("chatId",chatId)
-        intent.putExtra("user", user)
-        startActivity(intent)
+        /* PREGUNTAR COMO SE ENVÍA
+        setFragmentResult("ChatUsuario", bundleOf("usuario" to user, "chatId" to chatId))
+        findNavController().navigate(R.id.action_chats_to_mensaje)
+
+        println("si me imprimo $chatId")
+
+        //val accion = chatsDirections.actionChatsToMensaje(chatid)
+
          */
     }
 }
--- app/src/main/java/mx/itesm/dibujandounmaana/view/Mensaje.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/view/Mensaje.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import mx.itesm.dibujandounmaana.Adaptadores.MessageAdapter
 import mx.itesm.dibujandounmaana.databinding.NavMensajeBinding
+import mx.itesm.dibujandounmaana.model.Mensaje
 
 class Mensaje : Fragment() {
 
     private var chatId= ""
     private var user = ""
+
+    private var db = Firebase.firestore
 
     private lateinit var binding: NavMensajeBinding
 
@@ -24,14 +31,57 @@ class Mensaje : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        /*  Preguntar cómo llega
+        setFragmentResultListener("ChatUsuario"){requestKey, bundle ->
+        user = bundle.getString("usuario")!!
+        chatId = bundle.getString("chatId")!!}
+        println("si estoy $user")
+        initViews()
+         */
+    }
+
     private fun initViews(){
         binding.mensajeRecyclerView.layoutManager = LinearLayoutManager(this.requireContext())
         binding.mensajeRecyclerView.adapter = MessageAdapter(user)
 
+        println("Hola soy el chatId: $chatId")
+
         binding.btnMensaje.setOnClickListener { sendMessage() }
+
+        val chatRef = db.collection("chats").document(chatId)
+
+        println("si llegue 2")
+
+        chatRef.collection("messages").orderBy("dob", Query.Direction.ASCENDING)
+            .get()
+            .addOnSuccessListener { messages ->
+                val listMessages = messages.toObjects(Mensaje::class.java)
+                (binding.mensajeRecyclerView.adapter as MessageAdapter).setData(listMessages)
+            }
+
+        chatRef.collection("messages").orderBy("dob", Query.Direction.ASCENDING)
+            .addSnapshotListener { messages, error ->
+                if(error == null){
+                    messages?.let {
+                        val listMessages = it.toObjects(Mensaje::class.java)
+                        (binding.mensajeRecyclerView.adapter as MessageAdapter).setData(listMessages)
+                    }
+                }
+            }
+        println("si llegue 3")
     }
 
     private fun sendMessage(){
+        val message = Mensaje(
+            message = binding.campoMensaje.text.toString(),
+            from = user
+        )
+
+        db.collection("chats").document(chatId).collection("messages").document().set(message)
+
         binding.campoMensaje.setText("")
+
     }
 }
