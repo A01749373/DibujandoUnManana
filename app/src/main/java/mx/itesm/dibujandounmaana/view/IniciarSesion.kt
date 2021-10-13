9464c0c29948f367033104d140b48a09be0f4292
A01749448 <a01749448@itesm.mx>
2021-10-13 13:12:45 -0500

    Ajustes en pantalla iniciar sesión

--- app/src/main/java/mx/itesm/dibujandounmaana/view/IniciarSesion.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/view/IniciarSesion.kt
@@ -14,6 +14,7 @@ import mx.itesm.dibujandounmaana.R
 import mx.itesm.dibujandounmaana.model.SesionUsuario
 import mx.itesm.dibujandounmaana.databinding.IniciarSesionFragmentBinding
 import mx.itesm.dibujandounmaana.viewmodel.IniciarSesionVM
+import android.widget.Toast.makeText
 import kotlinx.coroutines.delay as delay
 
 class IniciarSesion : Fragment() {
@@ -21,6 +22,8 @@ class IniciarSesion : Fragment() {
     private val viewModel: IniciarSesionVM by viewModels()
 
     private lateinit var binding: IniciarSesionFragmentBinding
+
+    private var anuncioRespuesta: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,12 +51,15 @@ class IniciarSesion : Fragment() {
         }
     }
 
+    //if (binding.etUsuario.text.toString().isNotEmpty() && binding.etContrasena.text.isNotEmpty()) {
     private fun configurarEventos() {
         binding.btnIniciarSesion.setOnClickListener {
             val usuarioRegistrado = SesionUsuario(
                 binding.etUsuario.text.toString(),
-                binding.etContrasena.text.toString())
-            val preferencias = this.requireContext().getSharedPreferences("Usuario", Context.MODE_PRIVATE)
+                binding.etContrasena.text.toString()
+            )
+            val preferencias =
+                this.requireContext().getSharedPreferences("Usuario", Context.MODE_PRIVATE)
             preferencias.edit {
                 putString("Correo", binding.etUsuario.text.toString())
                 commit()
@@ -66,10 +72,16 @@ class IniciarSesion : Fragment() {
                 println("No funciono")
             }
             viewModel.verificaUsuario(usuarioRegistrado)
-            //delay(2000)
-            findNavController().navigate(R.id.action_iniciarSesion_to_nav_quienes)
+            if (viewModel.respuesta.value == "Lo sentimos: Usuario o contraseña no válidos") {
+                //Toast.makeText(activity, "${viewModel.respuesta.value}" + " 😭", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity, anuncioRespuesta + " 😭", Toast.LENGTH_SHORT).show()
+                //println(viewModel.respuesta.value)
+            } else {
+                //Toast.makeText(activity, "${viewModel.respuesta.value}" + " 😃", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity, anuncioRespuesta + " 😃", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_iniciarSesion_to_nav_quienes)
+            }
         }
-
         binding.btnCrearCuenta.setOnClickListener {
             findNavController().navigate(R.id.action_iniciarSesion_to_crear_cuenta)
         }
@@ -78,6 +90,7 @@ class IniciarSesion : Fragment() {
     private fun configurarObservadores() {
         viewModel.respuesta.observe(viewLifecycleOwner) { respuesta ->
             binding.tvEstado.text = respuesta
+            anuncioRespuesta = respuesta.toString()
         }
     }
 }
