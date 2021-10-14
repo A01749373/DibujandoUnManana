25df7be309bbdc861d828c69f60bab7565d32104
Ariadna Guzmán <58440787+A01749373@users.noreply.github.com>
2021-10-14 12:06:27 -0500

    Splash screen 💅🏻😀

--- app/src/main/java/mx/itesm/dibujandounmaana/MainActivity.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/MainActivity.kt
@@ -23,8 +23,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_DibujandoUnMañana_NoActionBar)
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
