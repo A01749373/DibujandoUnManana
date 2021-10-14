1fddae6fad238642fdc7b0cfe1d2638bb2c6af92
a01750147 <A01750147@itesm.mx>
2021-10-13 21:30:28 -0500

    Guardar Foto Perfil Firebase

--- app/src/main/java/mx/itesm/dibujandounmaana/view/PerfilUsuario.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/view/PerfilUsuario.kt
@@ -16,6 +16,12 @@ import mx.itesm.dibujandounmaana.R
 import android.content.Intent
 import android.widget.Button
 import android.widget.ImageView
+import android.app.ProgressDialog
+import com.google.firebase.storage.FirebaseStorage
+import android.net.Uri
+import android.widget.Toast
+import java.io.File
+import android.graphics.BitmapFactory
 
 
 
@@ -32,6 +38,7 @@ class perfilUsuario : Fragment() {
 
     private lateinit var button: Button
     private lateinit var imageView: ImageView
+    private lateinit var imageURi: Uri
 
 
     override fun onCreateView(
@@ -53,7 +60,37 @@ class perfilUsuario : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+            imageURi = data?.data!!
+            val progressDialog = ProgressDialog(this.requireContext())
+            progressDialog.setMessage("Subiendo archivo")
+            progressDialog.setCancelable(false)
+            progressDialog.show()
+            val preferencias = this.requireContext().getSharedPreferences("Usuario", Context.MODE_PRIVATE)
+            val correo = preferencias.getString("Correo", "-1")
+            if (correo != "-1") {
+                println("Este es el correo del usuario: $correo")
+            } else {
+                println("No funciono")
+            }
+            val fileName = correo
+            val storageReference = FirebaseStorage.getInstance().getReference("Imagenes/$fileName")
             imageView.setImageURI(data?.data)
+            storageReference.putFile(imageURi).
+                addOnSuccessListener {
+                    Toast.makeText(this.requireContext(),"Imagen cambiada",Toast.LENGTH_SHORT).show()
+                    if (progressDialog.isShowing){
+                        progressDialog.dismiss()
+                    }
+                    preferencias.edit {
+                        putBoolean("ImagenPerfil", true)
+                        commit()
+                    }
+                }.addOnFailureListener{
+                    if (progressDialog.isShowing){
+                        progressDialog.dismiss()
+                    }
+                Toast.makeText(this.requireContext(),"Error al subir la imagen",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -68,17 +105,16 @@ class perfilUsuario : Fragment() {
         val preferencias = this.requireContext().getSharedPreferences("Usuario", Context.MODE_PRIVATE)
         val correo = preferencias.getString("Correo","-1")
         viewModel.descargarDatosUsuario(correo.toString())
-
         binding.btnDonacion.setOnClickListener {
             findNavController().navigate(R.id.action_nav_perfil_to_lista_donaciones)
         }
-
         button = binding.btnEditarFoto
         imageView = binding.imgPerfil
-
         button.setOnClickListener {
             pickImageGallery()
         }
+
+
     }
 
     private fun configurarObservadores() {
@@ -86,6 +122,15 @@ class perfilUsuario : Fragment() {
             binding.tvNombre.text = respuesta.nombre
             binding.tvCorreo.text = respuesta.correo
             binding.tvHistorial.text = respuesta.historial.toString()
+        }
+        //Poner la imagen de perfil
+        val preferencias = this.requireContext().getSharedPreferences("Usuario", Context.MODE_PRIVATE)
+        val correo = preferencias.getString("Correo","-1").toString()
+        val storageRef = FirebaseStorage.getInstance().reference.child("Imagenes/$correo")
+        val archivo = File.createTempFile("temp","jpg")
+        storageRef.getFile(archivo).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(archivo.absolutePath)
+            binding.imgPerfil.setImageBitmap(bitmap)
         }
     }
 }
