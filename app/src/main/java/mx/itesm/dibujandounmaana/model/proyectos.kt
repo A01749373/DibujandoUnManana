9b77c11d9638b08a738c6b1c5161030559c300cd
A01750185 <A01750185@itesm.mx>
2021-10-14 19:22:58 -0500

    Agregar fotos a proyectos

--- app/src/main/java/mx/itesm/dibujandounmaana/model/proyectos.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/model/proyectos.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class Proyecto (
+    @SerializedName("idProyectos")
+    val idproyecto: Int,
     @SerializedName("nombreProyecto")
     val proyecto: String,
     @SerializedName("descripProyecto")
--- app/src/main/java/mx/itesm/dibujandounmaana/view/AdaptadorListaProyectos.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/view/AdaptadorListaProyectos.kt
@@ -1,22 +1,30 @@
 package mx.itesm.dibujandounmaana.view
 
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import mx.itesm.dibujandounmaana.R
 import mx.itesm.dibujandounmaana.model.Proyecto
+import com.google.firebase.storage.FirebaseStorage
+import mx.itesm.dibujandounmaana.databinding.NavProyectosBinding
+import java.io.File
+
 
 class AdaptadorListaProyectos (var arrProyectos: ArrayList<Proyecto>):
     RecyclerView.Adapter<AdaptadorListaProyectos.ProyectosViewHolder>()
 {
+    private lateinit var binding: NavProyectosBinding
 
     //Crea cada renglon
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProyectosViewHolder {
         //Cada renglon se crea aqui
         val vista = LayoutInflater.from(parent.context).inflate(R.layout.renglon_proyectos, parent, false)
         return ProyectosViewHolder(vista)
+
     }
 
     //En cada uno de los renglones
@@ -41,10 +49,20 @@ class AdaptadorListaProyectos (var arrProyectos: ArrayList<Proyecto>):
     class ProyectosViewHolder(vista: View): RecyclerView.ViewHolder(vista) {
         private val tvProyectos = vista.findViewById<TextView>(R.id.tvProyecto)
         private val tvDescripcion = vista.findViewById<TextView>(R.id.tvDescripcion)
+        private val ivImagen = vista.findViewById<ImageView>(R.id.ivProyectoImg)
 
         fun set(proyecto: Proyecto){
             tvProyectos.text = proyecto.proyecto
             tvDescripcion.text = proyecto.descrip
+            val storageRef = FirebaseStorage.getInstance().reference.child("ImagenesProyectos/${proyecto.idproyecto}.png")
+            val archivo = File.createTempFile("temp","png")
+            storageRef.getFile(archivo).addOnSuccessListener {
+                val bitmap = BitmapFactory.decodeFile(archivo.absolutePath)
+                ivImagen.setImageBitmap(bitmap)
+            }
+
+
         }
+
     }
 }
