118c780a2f5f38adb8b4621d290ead1109b51642
A01750185 <A01750185@itesm.mx>
2021-10-18 18:05:09 -0500

    CrearCuenta cambios trabajo en zoom

--- app/src/main/java/mx/itesm/dibujandounmaana/CrearCuentaAct.kt
+++ app/src/main/java/mx/itesm/dibujandounmaana/CrearCuentaAct.kt
@@ -84,54 +84,64 @@ class CrearCuentaAct : AppCompatActivity() {
     private fun configurarEventos() {
 
         binding.btnEvniar.setOnClickListener {
-            if (binding.etCorreo.text.toString().isNotEmpty() && binding.etNombreUsuario.text.toString().isNotEmpty()) {
-                if (binding.etYearFN.text.toString().length == 4) {
-                    if(binding.etMesFN.text.toString().toInt() >= 1 && binding.etMesFN.text.toString().toInt() <= 12){
-                        if(binding.etDiaFN.text.toString().toInt() >= 1 && binding.etDiaFN.text.toString().toInt() <= 31) {
-                            if (binding.etContrsena.text.toString().length > 6) {
-                                if (binding.etContrsena.text.toString() == binding.etconfcontra.text.toString()) {
-                                    val fecha =
-                                        "${binding.etYearFN.text.toString()}-${binding.etMesFN.text.toString()}-${binding.etDiaFN.text.toString()}"
-                                    val nuevoUsuario = Usuario(
-                                        binding.etCorreo.text.toString(),
-                                        binding.etNombreUsuario.text.toString(),
-                                        fecha,
-                                        binding.etGenero.selectedItem.toString(),
-                                        binding.etPais.selectedItem.toString(),
-                                        binding.etContrsena.text.toString()
-                                    )
+            if (binding.etCorreo.text.toString().isNotEmpty()) {
+                if (binding.etNombreUsuario.text.toString().isNotEmpty()) {
+                    if (binding.etYearFN.text.toString().length == 4) {
+                        if (binding.etMesFN.text.toString()
+                                .toInt() >= 1 && binding.etMesFN.text.toString().toInt() <= 12 &&
+                                binding.etMesFN.text.toString().isNotEmpty()
+                        ) {
+                            if (binding.etDiaFN.text.toString()
+                                    .toInt() >= 1 && binding.etDiaFN.text.toString().toInt() <= 31 &&
+                                    binding.etDiaFN.text.toString().isNotEmpty()
+                            ) {
+                                if (binding.etContrsena.text.toString().length > 6) {
+                                    if (binding.etContrsena.text.toString() == binding.etconfcontra.text.toString()) {
+                                        val fecha =
+                                            "${binding.etYearFN.text.toString()}-${binding.etMesFN.text.toString()}-${binding.etDiaFN.text.toString()}"
+                                        val nuevoUsuario = Usuario(
+                                            binding.etCorreo.text.toString(),
+                                            binding.etNombreUsuario.text.toString(),
+                                            fecha,
+                                            binding.etGenero.selectedItem.toString(),
+                                            binding.etPais.selectedItem.toString(),
+                                            binding.etContrsena.text.toString()
+                                        )
 
-                                    viewModel.enviarUsuario(nuevoUsuario)
-                                    auth.createUserWithEmailAndPassword(
-                                        binding.etCorreo.text.toString(),
-                                        binding.etContrsena.text.toString()
-                                    ).addOnCompleteListener(this) { task ->
-                                        if (task.isSuccessful) {
-                                            println("Usuario creado ${auth.currentUser}")
-                                            auth.currentUser!!.sendEmailVerification()
+                                        viewModel.enviarUsuario(nuevoUsuario)
+                                        auth.createUserWithEmailAndPassword(
+                                            binding.etCorreo.text.toString(),
+                                            binding.etContrsena.text.toString()
+                                        ).addOnCompleteListener(this) { task ->
+                                            if (task.isSuccessful) {
+                                                println("Usuario creado ${auth.currentUser}")
+                                                auth.currentUser!!.sendEmailVerification()
 
-                                        } else {
-                                            println("Fallido/n ${task.result.toString()}")
+                                            } else {
+                                                println("Fallido/n ${task.result.toString()}")
+                                            }
                                         }
+                                        abrirActividad()
+                                    } else {
+                                        binding.etconfcontra.setError("Las contraseñas no coinciden")
                                     }
-                                    abrirActividad()
                                 } else {
-                                    binding.etconfcontra.setError("Las contraseñas no coinciden")
+                                    binding.etContrsena.setError("La contraseña debe ser mayor a 6 caracteres")
                                 }
                             } else {
-                                binding.etContrsena.setError("La contraseña debe ser mayor a 6 caracteres")
+                                binding.etDiaFN.setError("El Día debe ser un dígito entre 1 y 31")
                             }
-                        } else{
-                            binding.etDiaFN.setError("El Día debe ser un dígito entre 1 y 31")
+                        } else {
+                            binding.etMesFN.setError("El mes debe ser un dígito entre 1 y 12")
                         }
-                    } else{
-                        binding.etMesFN.setError("El mes debe ser un dígito entre 1 y 12")
+                    } else {
+                        binding.etYearFN.setError("El año debe contener 4 dígitos")
                     }
                 } else {
-                    binding.etYearFN.setError("El año debe contener 4 dígitos")
+                    binding.etNombreUsuario.setError("No puedes dejar campos vacios")
                 }
             } else {
-                Toast.makeText(this,"No puedes dejar campos vacios", Toast.LENGTH_SHORT).show()
+                binding.etCorreo.setError("No puedes dejar campos vacios")
             }
         }
 
